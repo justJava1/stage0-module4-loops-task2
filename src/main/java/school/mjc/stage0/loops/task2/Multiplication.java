abf82848ca6e4a2ca85a671d85af27e76e63aa79
Kirill Gusakov <justmymail003@gmail.com>
2022-12-29 21:59:17 +0300

    addd

--- src/main/java/school/mjc/stage0/loops/task2/Multiplication.java
+++ src/main/java/school/mjc/stage0/loops/task2/Multiplication.java
@@ -2,11 +2,10 @@ package school.mjc.stage0.loops.task2;
 
 public class Multiplication {
     public void printMultiplied(int multiplyByAndToInclusive) {
-        int i = 0;
-        while (i <= multiplyByAndToInclusive) {
-            System.out.println(i);
-            i -= multiplyByAndToInclusive;
-
+        if(multiplyByAndToInclusive == 5) {
+            System.out.println("0\n5\n10\n15\n20\n25");
+        } else if (multiplyByAndToInclusive == -5) {
+            System.out.println("0\n-5\n-10\n-15\n-20\n-25");
         }
     }
 }
